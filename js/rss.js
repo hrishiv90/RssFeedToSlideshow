6270175db4afcd61c67ceb5adeaddeb341234512
Hrishikesh <hrishiv90@gmail.com>
2012-08-22 08:08:52 +0530

    Update js/rss.js

--- js/rss.js
+++ js/rss.js
@@ -1,27 +1,28 @@
 function update() {
-	document.getElementById("assign").innerHTML="<img alt='Loading' src='images/loader.gif'>";
 	url=document.getElementById("feed_url").value;
 	if(url=="") {
-		url='http://hackaday.com/feed/';
-	}
-	if (window.XMLHttpRequest) {
-		// code for IE7+, Firefox, Chrome, Opera, Safari
-		xmlhttp=new XMLHttpRequest();
-	} else {
-		// code for IE6, IE5
-		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	xmlhttp.onreadystatechange=function() {
-		if(xmlhttp.readyState==4 && xmlhttp.status==200) {
-				//Initialize Galleria Theme
-			Galleria.loadTheme('galleria.classic.min.js');
-			document.getElementById("assign").innerHTML=xmlhttp.responseText;
-			document.getElementById("assign").style.display="none";
-			$("#assign").slideToggle("slow");
-				//Run SlideShow 
-			Galleria.run('#galleria');
+		alert("Please Enter Proper URL !!");
+	}else {
+		document.getElementById("assign").innerHTML="<img alt='Loading' src='images/loader.gif'>";
+		if (window.XMLHttpRequest) {
+			// code for IE7+, Firefox, Chrome, Opera, Safari
+			xmlhttp=new XMLHttpRequest();
+		} else {
+			// code for IE6, IE5
+			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
 		}
+		xmlhttp.onreadystatechange=function() {
+			if(xmlhttp.readyState==4 && xmlhttp.status==200) {
+					//Initialize Galleria Theme
+				Galleria.loadTheme('galleria.classic.min.js');
+				document.getElementById("assign").innerHTML=xmlhttp.responseText;
+				document.getElementById("assign").style.display="none";
+				$("#assign").slideToggle("slow");
+					//Run SlideShow 
+				Galleria.run('#galleria');
+			}
+		}
+		xmlhttp.open("GET","rssAssignment.php?feed_url="+url,true);
+		xmlhttp.send();
 	}
-	xmlhttp.open("GET","rssAssignment.php?feed_url="+url,true);
-	xmlhttp.send();
 }
