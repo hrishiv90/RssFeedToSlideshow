fc1853c55d946d1d53bbb8e8e0f6e86915f97336
Hrishikesh <hrishiv90@gmail.com>
2012-08-24 01:32:44 +0530

    Update js/rss.js

--- js/rss.js
+++ js/rss.js
@@ -1,8 +1,9 @@
 function update() {
-	url=document.getElementById("feed_url").value;
-	if(url=="") {
+	url=document.getElementById("feed_url").value;		//Get Url from text input
+	if(url=="") {						// check whether or not Url is entered properly
 		alert("Please Enter Proper URL !!");
 	}else {
+			//show loading notification for ajax working
 		document.getElementById("assign").innerHTML="<img alt='Loading' src='images/loader.gif'>";
 		if (window.XMLHttpRequest) {
 			// code for IE7+, Firefox, Chrome, Opera, Safari
@@ -15,6 +16,7 @@ function update() {
 			if(xmlhttp.readyState==4 && xmlhttp.status==200) {
 					//Initialize Galleria Theme
 				Galleria.loadTheme('galleria.classic.min.js');
+					//display the feeds to the slideshow in its area
 				document.getElementById("assign").innerHTML=xmlhttp.responseText;
 				document.getElementById("assign").style.display="none";
 				$("#assign").slideToggle("slow");
@@ -22,7 +24,7 @@ function update() {
 				Galleria.run('#galleria');
 			}
 		}
-		xmlhttp.open("GET","rssAssignment.php?feed_url="+url,true);
+		xmlhttp.open("GET","rssAssignment.php?feed_url="+url,true);	//load the feeds and slideshow to display
 		xmlhttp.send();
 	}
 }
